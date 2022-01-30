590dda80cca0c177adbcebea2672c44337fce8e0
Aidan Hyland <Aidan.Hyland@fenergo.com>
2022-01-30 23:12:03 +0000

    Adding eslint & cy.task example

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,14 +1,22 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
 const browserify = require("@cypress/browserify-preprocessor");
 const tagify = require("cypress-tags");
+require("dotenv").config();
 
 module.exports = (on, config) => {
-  const options = {
-    ...browserify.defaultOptions,
-    typescript: require.resolve("typescript")
-  };
+    const options = {
+        ...browserify.defaultOptions,
+        typescript: require.resolve("typescript")
+    };
 
-  on("file:preprocessor", (file) => {
-    return file.filePath.includes(".feature") ? cucumber(options)(file) : tagify(config)(file);
-  });
+    on("file:preprocessor", (file) => {
+        return file.filePath.includes(".feature") ? cucumber(options)(file) : tagify(config)(file);
+    });
+
+    // cy.task allows to execute code in Node - Very powerful - Example: cy.task('getUsers')
+    on("task", {
+        getUsers: () => {
+            return { user1: process.env.TOM, user2: process.env.JERRY };
+        }
+    });
 };
